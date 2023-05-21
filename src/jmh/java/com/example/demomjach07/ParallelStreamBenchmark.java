e6f905c073e25caceee576adef8c0f58e7a75fb7
khanian <khanylee@gmail.com>
2023-05-22 05:35:20 +0900

    Initial commit

---
+++ src/jmh/java/com/example/demomjach07/ParallelStreamBenchmark.java
@@ -0,0 +1,25 @@
+package com.example.demomjach07;
+
+import org.openjdk.jmh.annotations.*;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(value = 2, jvmArgs = {"-Xms4G", "-Xmx4G"})
+public class ParallelStreamBenchmark {
+    private static final long N = 10_000_000L;
+
+    @Benchmark
+    public long sequentialSum() {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(N).reduce(0L, Long::sum);
+    }
+
+    @TearDown(Level.Invocation)
+    public void tearDown() {
+        System.gc();
+    }
+}
---
+++ src/jmh/java/com/example/demomjach07/ParallelStreams.java
@@ -0,0 +1,28 @@
+package com.example.demomjach07;
+
+import java.util.stream.Stream;
+
+public class ParallelStreams {
+
+    public long iterativeSum(long n) {
+        long result = 0;
+        for (long i = 1L; i <= n; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    public long sequentialSum(long n) {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(n)
+                .reduce(0L, Long::sum);
+    }
+
+    public long parallelSum(long n) {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(n)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+
+}
